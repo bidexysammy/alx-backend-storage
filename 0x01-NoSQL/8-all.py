ebbe6be0abbe6bb071905b8f7326de75ff5f1fb8
bidexysammy <bidexysammy@yahoo.com>
2023-07-18 15:27:07 -0700

    completed task

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """ This is a function that returns all the documents in a collection.
 """
-import pymongo
-import mongodb
 
 def list_all(mongo_collection):
     """args: takes a mongo collection as argument
